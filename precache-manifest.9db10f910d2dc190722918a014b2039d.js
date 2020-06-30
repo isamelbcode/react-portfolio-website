70c5027251b802f805cbfd0bac54534f9f08ecd1
isamelb <isamelb@gmail.com>
2020-06-30 15:03:44 +0100

    Updates

--- precache-manifest.9db10f910d2dc190722918a014b2039d.js
+++ precache-manifest.9db10f910d2dc190722918a014b2039d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-portfolio-website/static/js/runtime~main.13666a1d.js"
   },
   {
-    "revision": "a3c80ad75186dbb43205",
-    "url": "/react-portfolio-website/static/js/main.a3c80ad7.chunk.js"
+    "revision": "6318427eff70ab195e74",
+    "url": "/react-portfolio-website/static/js/main.6318427e.chunk.js"
   },
   {
     "revision": "5ec21d738fba382f2658",
     "url": "/react-portfolio-website/static/js/2.5ec21d73.chunk.js"
   },
   {
-    "revision": "a3c80ad75186dbb43205",
+    "revision": "6318427eff70ab195e74",
     "url": "/react-portfolio-website/static/css/main.2e429246.chunk.css"
   },
   {
-    "revision": "a5e4d6375a0f4811b3406b44c52ec951",
+    "revision": "3186f815143aa32f11d24d9d9926643b",
     "url": "/react-portfolio-website/index.html"
   }
 ];
